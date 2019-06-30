7ead2729242ac890353683f2adbbc948e86c3895
PranavG19 <41837262+PranavG19@users.noreply.github.com>
2019-06-29 20:21:52 -0400

    Tic Tac Toe Test

---
+++ src/main/java/com/example/myapplication/TicTacToe.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TicTacToe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tic_tac_toe);
+    }
+
+    private void back() {
+        Button back = findViewById()
+    }
+}
